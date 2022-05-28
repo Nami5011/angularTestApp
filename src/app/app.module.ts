0395cef175ef1713c481fcdb5f09affbce160811
Nami5011 <maron.011821@gmail.com>
2022-05-28 09:21:57 +0900

    routing, Home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+	{ path: '', component: HomeComponent }
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HomeComponent
+	],
+	imports: [
+		BrowserModule,
+		RouterModule.forRoot(routes)
+	],
+	exports: [RouterModule],
+	providers: [],
+	bootstrap: [AppComponent]
 })
+
 export class AppModule { }
---
+++ src/app/global/constants.ts
@@ -0,0 +1,3 @@
+export class Constants {
+	public static AppTitle: string = 'Angular Shipping';
+}
