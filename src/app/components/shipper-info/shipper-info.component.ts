af7228b5fdf9ce2120a8483671a0163e136c803e
Nami5011 <maron.011821@gmail.com>
2022-07-05 12:03:34 +0900

    add form shipper-info

--- src/app/components/shipper-info/shipper-info.component.ts
+++ src/app/components/shipper-info/shipper-info.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { DeliveryReq } from 'src/app/interface/delivery-req';
 
 @Component({
-  selector: 'app-shipper-info',
-  templateUrl: './shipper-info.component.html',
-  styleUrls: ['./shipper-info.component.css']
+	selector: 'app-shipper-info',
+	templateUrl: './shipper-info.component.html',
+	styleUrls: ['./shipper-info.component.css']
 })
 export class ShipperInfoComponent implements OnInit {
+	deliveryReq: DeliveryReq = JSON.parse(sessionStorage.getItem('deliveryReq') || String(new DeliveryReq()));
+	pickupFlag = false;
 
-  constructor() { }
+	constructor(private _router: Router) { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+		console.log('shipper-info component');
+	}
 
+	goBack() {
+		this._router.navigateByUrl('deliveryInfoMain');
+	}
 }
